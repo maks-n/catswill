cd0899874aaa494d6e4b8e6308b4edfaa3c5f052
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-03-18 02:14:16 +0100

    Add Catswill website

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Play a clip of a cat's meow when a user clicks the gpig button in the nav-bar.
+// Audio code learned about here: https://stackoverflow.com/questions/18826147/javascript-audio-play-on-click
+function play_sound(url) {
+  new Audio(url).play();
+}
